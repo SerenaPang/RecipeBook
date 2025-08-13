6ada1a1f21c75285334a0cffa268b3dd6094f72f
SerenaPang4 <ypang4@usfca.edu>
2025-08-13 16:09:53 -0700

    added entity classes recipe and ingredient

---
+++ recipe-app/src/main/java/com/recipebook/entity/Recipe.java
@@ -0,0 +1,36 @@
+package com.recipebook.entity;
+
+public class Recipe {
+	private String name;
+	private String description;
+	private String type;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Recipe [name=" + name + ", description=" + description + ", type=" + type + "]";
+	}
+}
