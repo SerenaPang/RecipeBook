2edcf037b8e2f4effa988b6b6a6fbc3192d6eba0
SerenaPang4 <ypang4@usfca.edu>
2025-08-13 15:37:07 -0700

    added project configuratino

---
+++ recipe-app/src/main/java/com/recipebook/app/ProjectConfig.java
@@ -0,0 +1,10 @@
+package com.recipebook.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.recipebook.app"})
+public class ProjectConfig {
+
+}
