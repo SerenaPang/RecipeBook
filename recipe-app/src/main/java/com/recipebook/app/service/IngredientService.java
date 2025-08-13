9ebbbca82c01b7ecdfc5dfc0ded4c38a63e60c37
SerenaPang4 <ypang4@usfca.edu>
2025-08-13 16:35:51 -0700

    added indredient service

--- recipe-app/src/main/java/com/recipebook/app/service/IngredientService.java
+++ recipe-app/src/main/java/com/recipebook/app/service/IngredientService.java
@@ -1,5 +1,20 @@
 package com.recipebook.app.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+import com.recipebook.entity.Ingredient;
+
+@Service
+@ApplicationScope
 public class IngredientService {
+	List<Ingredient> ingredients = new ArrayList<>();
+	public Ingredient addIngredient(Ingredient ingredient) {
+		ingredients.add(ingredient);
+		return ingredient;
+	}
 
 }
